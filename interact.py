70d0d32a6a7d2d4c56efe9eb014c9c1174256f26
Sean Silva <chisophugis@gmail.com>
2015-03-16 00:45:17 -0700

    Checkpoint

--- interact.py
+++ interact.py
@@ -8,40 +8,71 @@ def main():
 
     ser = serial.Serial('/dev/ttyUSB0')
 
-    # See UM10601 "LPC81x User manual"
+    # See UM10601: "LPC81x User manual"
     # 21.6.2 Boot process
 
     ser.write('?')
     s = ser.readline()
-    print('Read: {!r}'.format(s))
     assert s.strip() == 'Synchronized'
 
-    ser.write('Synchronized\r\n')
+    w = lambda s: write_with_echo(ser, s + '\r\n')
+    w('Synchronized')
     s = ser.readline()
-    print('Read: {!r}'.format(s))
-    assert s == 'Synchronized\r\n'
-    s = ser.readline()
-    print('Read: {!r}'.format(s))
     assert s.strip() == 'OK'
 
-    ser.write('12000\r\n')
-    s = ser.readline()
-    print('Read: {!r}'.format(s))
-    assert s == '12000\r\n'
+    w('12000')
     s = ser.readline()
     print('Read: {!r}'.format(s))
     assert s == 'OK\r\n'
 
-    ser.write('N\r\n')
-    s = ser.readline()
-    print('Read: {!r}'.format(s))
-    assert s == 'N\r\n'
+    uid = cmd_N(ser)
+    print(uid)
+
+    bootcode_version = cmd_K(ser)
+    print(bootcode_version)
+
+    cmd_U(ser)
+
+def write_with_echo(ser, s):
+    print('write_with_echo(ser, {!r})'.format(s))
+    print(' '*8 + 'sent: {!r}'.format(s))
+    ser.write(s)
+    echoed = ser.readline()
+    print(' '*8 + 'echo: {!r}'.format(echoed))
+    assert echoed == s
+
+
+# See UM10601: "LPC81x User manual"
+# 22.5.1 "UART ISP commands"
+
+def cmd_N(ser):
+    w = lambda s: write_with_echo(ser, s + '\r\n')
+    w('N')
     s = ser.readline()
     assert s == '0\r\n' # CMD_SUCCESS == 0
     l = []
     for i in range(4):
-        s = ser.readline()
-        l.append(int(s.strip()))
-    print([hex(x) for x in l])
+        line = ser.readline()
+        l.append(int(line.strip()))
+    return l
+
+def cmd_K(ser):
+    w = lambda s: write_with_echo(ser, s + '\r\n')
+    w('K')
+    s = ser.readline()
+    assert s == '0\r\n' # CMD_SUCCESS == 0
+    ret = []
+    for i in range(2):
+        line = ser.readline()
+        ret.append(int(line.strip()))
+    return ret
+
+def cmd_U(ser):
+    w = lambda s: write_with_echo(ser, s + '\r\n')
+    w('U 23130')
+    s = ser.readline()
+    assert s == '0\r\n' # CMD_SUCCESS == 0
+
+
 
 main()
